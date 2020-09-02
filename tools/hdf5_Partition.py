4b27aefb803e9ddd6be5d6a6d0c579181ef34b2c
cacsphysics <ccartagena@brynmawr.edu>
2020-09-02 18:54:35 -0500

    Update hdf5_Partition.py
    Adding a new function called 'dedalus_parent_attributes'. This gives and updates the hdf5 top-level attributes.

--- tools/hdf5_Partition.py
+++ tools/hdf5_Partition.py
@@ -32,6 +32,24 @@ def copy_attributes(in_object, out_object):
         print(value)
     return None
 
+def dedalus_parent_attributes(index, in_object, out_object, handler_name = None):
+    '''Copy attributes between 2 HDF5 objects.'''
+    for key, value in in_object.attrs.items():
+        if (key == 'handler_name'):
+            print(value)
+            if (handler_name == None):
+                out_object.attrs[key] = value + '_parition'
+            else:
+                out_object.attrs[key] = str(handler_name)
+            print(out_object.attrs[key])
+        elif (key == 'writes'):
+            print(value)
+            print(index)
+            out_object.attrs[key] = index
+        else:
+            out_object.attrs[key] = value
+    return None
+
 def dataset_Group_Name(full_Path, key_Value):
     """
         Output the group path of key_Value
@@ -66,6 +84,7 @@ def update_Tuple(tuple_Object, value_Update):
     
 def partition_Structure(index, h5file1, h5file2):
     '''Goes through entire h5file1 structure and copy into h5file2 with index alteration.'''
+    dedalus_parent_attributes(index, h5file1, h5file2, handler_name = handler_name)
     for key in h5file1.keys():
         if (type(h5file1[key]) == h5._hl.group.Group):
             group_path = h5file1[key].name    
